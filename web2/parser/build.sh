da3882320586c1643e9a619b2958565b7670521f
sigmanuts <supergng@gngidk.xyz>
2026-04-25 22:19:08 -0300

    faster backend compilation(linux only), faster client runtime speed(simd, bulk mem)

--- web2/parser/build.sh
+++ web2/parser/build.sh
@@ -1,1 +1,2 @@
-RUSTFLAGS="-g" wasm-pack build --target web --dev
+RUSTFLAGS="-g" wasm-pack build --target web --dev 
+#gng, -g = debug = big ass wasm, remove ts unless u wanna give them the full ass names to anyone
